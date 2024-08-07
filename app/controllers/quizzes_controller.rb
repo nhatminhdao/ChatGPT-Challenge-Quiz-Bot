2ef280de05267aaadcc364d0fe6162cff7efcb98
nhatminhdao <nhatminhdao@gmail.com>
2024-08-07 23:41:25 +0000

    form to create new topic

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -5,4 +5,19 @@ class QuizzesController < ApplicationController
 
     render({ :template => "index" })
   end
+
+  def show
+    # @the_id = params.fetch(":path_id")
+
+
+    render({ :template => "show" })
+  end
+
+  def create
+    new_quiz = Quiz.new
+    new_quiz.topic = params.fetch("query_topic")
+    new_quiz.save
+
+    redirect_to("/quizzes")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get("/", {:controller => "quizzes", :action => "index"})
   get("/quizzes", {:controller => "quizzes", :action => "index"})
-
+  get("/quizzes/:path_id", {:controller => "quizzes", :action => "show"})
+  post("/insert_quiz", {:controller => "quizzes", :action => "create"})
+  
   
 end
